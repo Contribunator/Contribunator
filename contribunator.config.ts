38f3c4dc4a9be857dd4949ff11dc369dae6dafd3
Istora Mandiri <1696942+IstoraMandiri@users.noreply.github.com>
2023-05-19 00:26:53 +0000

    dev config

--- contribunator.config.ts
+++ contribunator.config.ts
@@ -1,6 +1,20 @@
 import { AppConfig } from "@/util/config";
 
 // override default config here, see src/config.ts for defaults
-const config: AppConfig = {};
+const config: AppConfig = {
+  repos: {
+    Another: {
+      title: "Testing",
+      contributions: [
+        {
+          title: "Tweet",
+          color: "blue",
+          description: "Tweet about this project",
+          type: "tweet",
+        },
+      ],
+    },
+  },
+};
 
 export default config;
