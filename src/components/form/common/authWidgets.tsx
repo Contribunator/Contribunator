30d03ee4c53747b6a732f8a1ccbc3180e3cb2f8a
Istora Mandiri <1696942+IstoraMandiri@users.noreply.github.com>
2023-06-19 12:42:23 +0000

    Add no-anon ui

--- src/components/form/common/authWidgets.tsx
+++ src/components/form/common/authWidgets.tsx
@@ -1,7 +1,23 @@
 import Captcha from "./captcha";
 import type { BaseFormProps } from "../withFormik";
+import { HiExclamation } from "react-icons/hi";
 
-export default function AuthWidgets({ formik }: BaseFormProps) {
+export default function AuthWidgets({ formik, config }: BaseFormProps) {
+  if (
+    formik.values.authorization === "anon" &&
+    !config.authorization.includes("anon")
+  ) {
+    return (
+      <div className="absolute inset-0 backdrop-blur-sm bg-base-100 bg-opacity-20 z-10 !m-0">
+        <div className="m-10 p-6 font-bold space-y-4 bg-base-100 rounded-xl bg-opacity-80">
+          <div className="text-3xl">
+            <HiExclamation className="inline" />
+          </div>
+          <div>You must sign in to submit this type of contribution</div>
+        </div>
+      </div>
+    );
+  }
   if (formik.values.authorization === "captcha") {
     return <Captcha />;
   }
--- src/components/form/common/commonOptions.tsx
+++ src/components/form/common/commonOptions.tsx
@@ -10,26 +10,24 @@ export default function GenericOptions({ formik, config }: BaseFormProps) {
       };
   return (
     <>
-      <div className="">
-        <div className="collapse collapse-arrow rounded-md bg-base-100 bg-opacity-50">
-          <input type="checkbox" className="peer" />
-          <div className="collapse-title text-left text-sm flex items-center peer-checked:font-bold">
-            Advanced Options
-          </div>
-          <div className="collapse-content space-y-6 -mx-1">
-            <TextInput
-              title="Custom Pull Request Title"
-              info="Special characters will be removed"
-              name="customTitle"
-              placeholder={meta.title}
-            />
-            <TextInput
-              title="Custom Pull Request Message"
-              name="customMessage"
-              placeholder={meta.message}
-              as="textarea"
-            />
-          </div>
+      <div className="collapse collapse-arrow rounded-md bg-base-100 bg-opacity-50">
+        <input type="checkbox" className="peer" />
+        <div className="collapse-title text-left text-sm flex items-center peer-checked:font-bold">
+          Advanced Options
+        </div>
+        <div className="collapse-content space-y-6 -mx-1">
+          <TextInput
+            title="Custom Pull Request Title"
+            info="Special characters will be removed"
+            name="customTitle"
+            placeholder={meta.title}
+          />
+          <TextInput
+            title="Custom Pull Request Message"
+            name="customMessage"
+            placeholder={meta.message}
+            as="textarea"
+          />
         </div>
       </div>
     </>
