bd8a8e614619a552340b6da7497feb2e352c9d1e
Istora Mandiri <1696942+IstoraMandiri@users.noreply.github.com>
2023-06-19 12:14:19 +0000

    Improve slugify with line-breaks

--- src/lib/helpers/slugify.ts
+++ src/lib/helpers/slugify.ts
@@ -9,9 +9,11 @@ export default function slugify(
     .toLowerCase()
     .replace(/https?:\/\/([^\/\s]+)(\/\S*)?/g, "$1") // strip long urls
     .replace(/[^a-z0-9 -]/g, "") // remove any non-alphanumeric characters
+    .replace(/(\r\n|\n|\r)/gm, "-") // replace line breaks with hyphens
     .replace(/\s+/g, "-") // replace spaces with hyphens
     .replace(/-+/g, "-") // remove consecutive hyphens
     .split("-") // split into an array
+    .filter((word) => word) // remove empty words
     .slice(0, slice) // slice to the first n words
     .join(join); // join into a string
 
