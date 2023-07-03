a03199786053b0c1a120efb421cb0ca78ae332b2
Istora Mandiri <1696942+IstoraMandiri@users.noreply.github.com>
2023-07-03 02:15:43 +0000

    Diable logtail

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -1,14 +1,15 @@
 import pino from "pino";
 
 function getLogger() {
-  if (process.env.LOGTAIL_SOURCE_TOKEN) {
-    return pino(
-      pino.transport({
-        target: "@logtail/pino",
-        options: { sourceToken: process.env.LOGTAIL_SOURCE_TOKEN },
-      })
-    );
-  }
+  // TODO enable once logtail in workers is working
+  // if (process.env.LOGTAIL_SOURCE_TOKEN) {
+  //   return pino(
+  //     pino.transport({
+  //       target: "@logtail/pino",
+  //       options: { sourceToken: process.env.LOGTAIL_SOURCE_TOKEN },
+  //     })
+  //   );
+  // }
   return pino({
     level: process.env.CI ? "silent" : "trace",
   });
