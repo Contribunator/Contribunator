d855869dd5eb89fe04ac6640dd00d9b10b6c1896
Istora Mandiri <1696942+IstoraMandiri@users.noreply.github.com>
2023-07-03 06:27:12 +0000

    Attempt to fix logging

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -1,15 +1,16 @@
 import pino from "pino";
+import "@logtail/pino";
 
 function getLogger() {
   // TODO enable once logtail in workers is working
-  // if (process.env.LOGTAIL_SOURCE_TOKEN) {
-  //   return pino(
-  //     pino.transport({
-  //       target: "@logtail/pino",
-  //       options: { sourceToken: process.env.LOGTAIL_SOURCE_TOKEN },
-  //     })
-  //   );
-  // }
+  if (process.env.LOGTAIL_SOURCE_TOKEN) {
+    return pino(
+      pino.transport({
+        target: "@logtail/pino",
+        options: { sourceToken: process.env.LOGTAIL_SOURCE_TOKEN },
+      })
+    );
+  }
   return pino({
     level: process.env.CI ? "silent" : "trace",
   });
