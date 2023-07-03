0c9b34d35c1e3b5f7081690e667cbd9090e099b0
Istora Mandiri <1696942+IstoraMandiri@users.noreply.github.com>
2023-07-03 02:04:30 +0000

    Pino logtail config

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -1,7 +1,17 @@
 import pino from "pino";
 
-const log = pino({
-  level: process.env.CI ? "silent" : "trace",
-});
+function getLogger() {
+  if (process.env.LOGTAIL_SOURCE_TOKEN) {
+    return pino(
+      pino.transport({
+        target: "@logtail/pino",
+        options: { sourceToken: process.env.LOGTAIL_SOURCE_TOKEN },
+      })
+    );
+  }
+  return pino({
+    level: process.env.CI ? "silent" : "trace",
+  });
+}
 
-export default log;
+export default getLogger();
